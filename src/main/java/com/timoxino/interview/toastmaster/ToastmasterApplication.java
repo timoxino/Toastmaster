43bfdce724433f2ece51a2aa73e3ee591984da04
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-23 16:43:02 -0400

    initial commit

---
+++ src/main/java/com/timoxino/interview/toastmaster/ToastmasterApplication.java
@@ -0,0 +1,13 @@
+package com.timoxino.interview.toastmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToastmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToastmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timoxino/interview/toastmaster/ToastmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timoxino.interview.toastmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToastmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
