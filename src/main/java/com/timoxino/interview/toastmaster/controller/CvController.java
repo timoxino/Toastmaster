5c8e0b2df164f9bcce6addeccca71ade46ec6e44
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-01 11:24:05 -0400

    implement email service

--- src/main/java/com/timoxino/interview/toastmaster/controller/CvController.java
+++ src/main/java/com/timoxino/interview/toastmaster/controller/CvController.java
@@ -1,18 +1,25 @@
 package com.timoxino.interview.toastmaster.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.timoxino.interview.shared.dto.CandidateQuestionsMessage;
 import com.timoxino.interview.toastmaster.dto.CvProcessingRequest;
 import com.timoxino.interview.toastmaster.dto.CvProcessingResponse;
+import com.timoxino.interview.toastmaster.service.OrchestrationService;
 
 @RestController
 @RequestMapping("/cv")
 public class CvController {
 
+    @Autowired
+    OrchestrationService orchestrationService;
+
     @PostMapping
     public CvProcessingResponse submitCvProcessing(CvProcessingRequest request) {
-        return CvProcessingResponse.builder().caseNumber("777").build();
+        orchestrationService.processQuestionsMessage(new CandidateQuestionsMessage());
+        return CvProcessingResponse.builder().caseNumber("email sent").build();
     }
 }
--- src/test/java/com/timoxino/interview/toastmaster/ToastmasterApplicationTests.java
+++ src/test/java/com/timoxino/interview/toastmaster/ToastmasterApplicationTests.java
@@ -2,8 +2,10 @@ package com.timoxino.interview.toastmaster;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = { "TOASTMASTER_EMAIL_FROM = test@gmail.com", "TOASTMASTER_EMAIL_TO = test@gmail.com" })
 class ToastmasterApplicationTests {
 
 	@Test
