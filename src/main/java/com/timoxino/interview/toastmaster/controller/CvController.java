dabd2597a845504232d3dfa3068ecdf411e22077
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-06 14:26:48 -0400

    add @RequestBody annotation

--- src/main/java/com/timoxino/interview/toastmaster/controller/CvController.java
+++ src/main/java/com/timoxino/interview/toastmaster/controller/CvController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.WritableResource;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class CvController {
     private Resource cvBucket;
 
     @PostMapping
-    public CvProcessingResponse submitCvProcessing(CvProcessingRequest request) throws IOException {
+    public CvProcessingResponse submitCvProcessing(@RequestBody CvProcessingRequest request) throws IOException {
 
         Resource file = cvBucket.createRelative("/testFile.txt");
         try (OutputStream os = ((WritableResource) file).getOutputStream()) {
