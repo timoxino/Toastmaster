33e806437f13124d0e6dab457da02bf8d88c7a50
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-07 17:08:28 -0400

    create randomly named files

--- src/main/java/com/timoxino/interview/toastmaster/service/OrchestrationService.java
+++ src/main/java/com/timoxino/interview/toastmaster/service/OrchestrationService.java
@@ -1,10 +1,14 @@
 package com.timoxino.interview.toastmaster.service;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.timoxino.interview.shared.dto.CandidateQuestionsMessage;
+import com.timoxino.interview.toastmaster.dto.CvProcessingRequest;
 
 @Service
 public class OrchestrationService {
@@ -20,11 +24,20 @@ public class OrchestrationService {
     @Autowired
     EmailService emailService;
 
+    @Autowired
+    StorageService storageService;
+
     public OrchestrationService(EmailService emailService) {
         this.emailService = emailService;
     }
 
     public void processQuestionsMessage(CandidateQuestionsMessage message) {
         emailService.sendEmail(from, to, SUBJECT_QUESTIONS_READY, "questions");
+    }
+
+    public String processCvRequest(CvProcessingRequest request) throws IOException {
+        String fileName = UUID.randomUUID().toString() + ".txt";
+        storageService.writeCvFile(fileName, request.getCvContent());
+        return fileName;
     }
 }
