aeef09da1d823293e288cb7d5a77eb4a987c5565
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-10 12:26:52 -0400

    add logging to orchestrator

--- src/main/java/com/timoxino/interview/toastmaster/service/OrchestrationService.java
+++ src/main/java/com/timoxino/interview/toastmaster/service/OrchestrationService.java
@@ -3,6 +3,8 @@ package com.timoxino.interview.toastmaster.service;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,8 @@ import com.timoxino.interview.toastmaster.spring.PubSubSenderConfiguration.PubSu
 public class OrchestrationService {
 
     final static String SUBJECT_QUESTIONS_READY = "Questions are ready!";
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(OrchestrationService.class);
 
     @Value("${toastmaster.email.from}")
     String from;
@@ -32,7 +36,8 @@ public class OrchestrationService {
     @Autowired
     PubSubCvGateway pubSubCvGateway;
 
-    public OrchestrationService(EmailService emailService, StorageService storageService, PubSubCvGateway pubSubCvGateway) {
+    public OrchestrationService(EmailService emailService, StorageService storageService,
+            PubSubCvGateway pubSubCvGateway) {
         this.emailService = emailService;
         this.storageService = storageService;
         this.pubSubCvGateway = pubSubCvGateway;
@@ -40,18 +45,23 @@ public class OrchestrationService {
 
     public void processQuestionsMessage(CandidateQuestionsMessage message) {
         emailService.sendEmail(from, to, SUBJECT_QUESTIONS_READY, "questions");
+        LOGGER.info("Email sent with the questions compiled for CV file: " + message.getCvUri());
     }
 
     public String processCvRequest(CvProcessingRequest request) throws IOException {
+        LOGGER.info("CV processing started.");
+
         String fileName = UUID.randomUUID().toString() + ".txt";
         storageService.writeCvFile(fileName, request.getCvContent());
+        LOGGER.info("File for CV created with the name: " + fileName);
 
         CandidateBaseMessage message = new CandidateBaseMessage();
         message.setCvUri(fileName);
         message.setLvlExpected(request.getLevelExpected());
         message.setRole(request.getRoleName());
         pubSubCvGateway.sendCvToPubSub(message);
-        
+        LOGGER.info("Message to PubSub sent with CV file name: " + fileName);
+
         return fileName;
     }
 }
