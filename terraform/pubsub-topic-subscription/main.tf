8e2808682698691b55b0febfdf69cd19e74614be
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-28 18:45:29 -0400

    deliver message once

--- terraform/pubsub-topic-subscription/main.tf
+++ terraform/pubsub-topic-subscription/main.tf
@@ -5,4 +5,5 @@ resource "google_pubsub_topic" "topic_compiled_questions" {
 resource "google_pubsub_subscription" "subscription_toastmaster" {
   name  = "toastmaster_subscription"
   topic = google_pubsub_topic.topic_compiled_questions.name
+  enable_exactly_once_delivery = true
 }
