c099f811d28a3949215c6ed51384b4ec22ab0f81
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-10-17 07:42:38 -0400

    add jackson-guava mapper

--- src/main/java/com/timoxino/interview/toastmaster/spring/Configurataion.java
+++ src/main/java/com/timoxino/interview/toastmaster/spring/Configurataion.java
@@ -3,14 +3,23 @@ package com.timoxino.interview.toastmaster.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.timoxino.interview.shared.dto.CandidateBaseMessage;
 import com.timoxino.interview.toastmaster.annotation.LocalProfile;
 import com.timoxino.interview.toastmaster.spring.PubSubSenderConfiguration.PubSubCvGateway;
 
 @Configuration
-@LocalProfile
 public class Configurataion {
 
+    @Bean
+    public ObjectMapper customizeJacksonConfiguration() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new GuavaModule());
+        return objectMapper;
+    }
+
+    @LocalProfile
     @Bean
     public PubSubCvGateway pubSubCvGateway() {
         return new PubSubCvGateway() {
