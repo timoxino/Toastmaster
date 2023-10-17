30a52c86e7cbc9c42f489060f8dcb6adb816fed2
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-10-17 07:42:38 -0400

    add jackson-guava mapper

--- src/main/java/com/timoxino/interview/toastmaster/annotation/LocalProfile.java
+++ src/main/java/com/timoxino/interview/toastmaster/annotation/LocalProfile.java
@@ -1,11 +1,14 @@
 package com.timoxino.interview.toastmaster.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
 @ConditionalOnProperty(name = "spring.profiles.active", havingValue = "local", matchIfMissing = true)
 public @interface LocalProfile {
 }
